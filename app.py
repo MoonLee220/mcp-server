d649fc47644c338426ccd4700786ced51ffc9e66
MoonLee220 <gina930418@gmail.com>
2026-03-17 15:11:44 +0800

    Add MCP server with add tool

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from fastmcp import FastMCP
 app=FastMCP("My MCP Server")
 #提供一個加法工具
-@app.tool
-def add(n1:int,n2:int) ->int;
+@app.tool()
+def add(n1:int,n2:int) ->int:
     """Add Two Number"""
     return n1+n2
+
+app.run() # 啟動 MCP 伺服器 
